5e1bab4a1461a484f110149a6ffbeeae444a1da5
herman.ras.it@gmail.com <herman.ras.it@gmail.com>
2021-02-08 21:12:05 +0200

    working online offline Icon

---
+++ API/task.php
@@ -0,0 +1,49 @@
+<?php
+// TEST to see if API is present
+if (isset($_GET['KEY'])){
+    // TEST to see if this is our key
+    if ($_GET['KEY'] == 'MucJIL1vkG6YJibwB7HINgvnT89gpK'){
+
+        // All good do some work
+        $sql = "SELECT * FROM task;";
+        require_once 'config/db_query.php';
+        $sqlargs = array();
+        $result = sqlQuery($sql, $sqlargs);
+
+        //is results for excel or Offline app?
+        if (isset($_GET['EXCEL'])){
+            //for excel use
+            echo '<table style="border:1px solid black;width:100%">';
+            echo '    <thead>';
+            echo '        <tr style="border:1px solid black;">';
+            echo '            <th style="border:1px solid black;">ID</th>';
+            echo '            <th style="border:1px solid black;">naam</th>';
+            echo '            <th style="border:1px solid black;">afkorting</th>';
+            echo '        </tr>';
+            echo '    </thead>';
+            echo '    <tbody>';
+
+            // repeat for all records
+            foreach ($result[0] as $row) {
+                echo "        <tr>";
+                echo "            <td style=\"border:1px solid black;\">".$row['id']."</td>";
+                echo "            <td style=\"border:1px solid black;\">".$row['naam']."</td>";
+                echo "            <td style=\"border:1px solid black;\">".$row['afkorting']."</td>";
+                echo "        </tr>";
+            }
+
+            echo '    </tbody>';
+            echo '</table>';
+        }else{
+            //for application use
+            echo json_encode($result[0]);
+        }
+
+    }else{
+        $err = array('error'=>'API AUTH KEY FAILED');
+        echo   json_encode($err);
+    }
+}else{
+    $err = array('error'=>'NO API KEY');
+    echo   json_encode($err);
+}
--- App/app.js
+++ App/app.js
@@ -1,7 +1,4 @@
-// Import DB Classes
-import { User } from "./user_class.js";
-import { Worker } from "./worker_class.js";
-import { Farm } from "./farm_class.js";
+// Import DB Functions
 import { getDBUpdate } from './dbAPI.js';
 
 // Setup DB if not setup
@@ -14,8 +11,8 @@ if (localStorage.getItem('worker') === null) {
     window.localStorage.setItem('worker', JSON.stringify([]));
 }
 // for farm
-if (localStorage.getItem('farm') === null) {
-    window.localStorage.setItem('farm', JSON.stringify([]));
+if (localStorage.getItem('plaas') === null) {
+    window.localStorage.setItem('plaas', JSON.stringify([]));
 }
 // for spilpunt
 if (localStorage.getItem('spilpunt') === null) {
@@ -34,19 +31,48 @@ if (localStorage.getItem('access') === null) {
     window.localStorage.setItem('access', JSON.stringify([]));
 }
 
-if (window.navigator.onLine) {
-    let OnlineStatus = true;
-    let baseURL = 'https://phq-7hxllh2.petragroup.local/web_dev/Projects/WildeklawerTydkaarte';
-    let apiKey = 'MucJIL1vkG6YJibwB7HINgvnT89gpK';
+function updateAppStatus(params) {
+    if (window.navigator.onLine) {
+        // set new notice
+        let old_notice = document.getElementById('notice');
+        if (old_notice) {
+            old_notice.remove();
+        }
 
-    // update tables system is online
-    getDBUpdate(baseURL + '/API/users.php' + '?KEY=' + apiKey, 'user');
-    getDBUpdate(baseURL + '/API/workers.php' + '?KEY=' + apiKey, 'worker');
-    getDBUpdate(baseURL + '/API/plaas.php' + '?KEY=' + apiKey, 'plaas');
-    getDBUpdate(baseURL + '/API/spilpunt.php' + '?KEY=' + apiKey, 'spilpunt');
-    getDBUpdate(baseURL + '/API/gewas.php' + '?KEY=' + apiKey, 'gewas');
-    getDBUpdate(baseURL + '/API/task.php' + '?KEY=' + apiKey, 'access');
+        // set api stuff
+        let baseURL = 'https://phq-7hxllh2.petragroup.local/web_dev/Projects/WildeklawerTydkaarte';
+        let apiKey = 'MucJIL1vkG6YJibwB7HINgvnT89gpK';
 
-} else {
-    let OnlineStatus = false;
+        // update tables system is online
+        getDBUpdate(baseURL + '/API/users.php' + '?KEY=' + apiKey, 'user');
+        getDBUpdate(baseURL + '/API/workers.php' + '?KEY=' + apiKey, 'worker');
+        getDBUpdate(baseURL + '/API/plaas.php' + '?KEY=' + apiKey, 'plaas');
+        getDBUpdate(baseURL + '/API/spilpunt.php' + '?KEY=' + apiKey, 'spilpunt');
+        getDBUpdate(baseURL + '/API/gewas.php' + '?KEY=' + apiKey, 'gewas');
+        getDBUpdate(baseURL + '/API/task.php' + '?KEY=' + apiKey, 'task');
+        getDBUpdate(baseURL + '/API/access.php' + '?KEY=' + apiKey, 'access');
+    } else {
+        // remove old notice if it was set
+        let old_notice = document.getElementById('notice');
+        if (old_notice) {
+            old_notice.remove;
+        }
+
+        // set new notice
+        let body = document.body;
+        let firstItem = document.body.children[0];
+        let notice = document.createElement("div");
+        notice.id = 'notice';
+        notice.classList.add("AppOffline");
+        notice.innerHTML = 'OffLine';
+        body.insertBefore(notice, firstItem);
+    }
+
 }
+
+window.addEventListener('offline', function (event) {
+    updateAppStatus('offline');
+});
+window.addEventListener('online', function (event) {
+    updateAppStatus('online');
+});
