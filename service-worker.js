6e138addf1e8eaae5f96c2f9b3966adcb5332de5
herman.ras.it@gmail.com <herman.ras.it@gmail.com>
2021-02-09 21:33:46 +0200

    working app & service worker

--- service-worker.js
+++ service-worker.js
@@ -1,29 +1,33 @@
-/*
-Copyright 2015, 2019 Google Inc. All Rights Reserved.
- Licensed under the Apache License, Version 2.0 (the "License");
- you may not use this file except in compliance with the License.
- You may obtain a copy of the License at
- http://www.apache.org/licenses/LICENSE-2.0
- Unless required by applicable law or agreed to in writing, software
- distributed under the License is distributed on an "AS IS" BASIS,
- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- See the License for the specific language governing permissions and
- limitations under the License.
-*/
-
 // Incrementing OFFLINE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 const OFFLINE_VERSION = 1;
 const CACHE_NAME = 'offline';
-// Customize this with a different URL if needed.
+
+// Customize offline page for any url not in cache
 const OFFLINE_URL = '/web_dev/Projects/WildeklawerTydkaarte/indexOffline.html';
+// list of urls to cache
+const filesToCache = [
+    '/web_dev/Projects/WildeklawerTydkaarte/',
+    '/web_dev/Projects/WildeklawerTydkaarte/CSS/app.css',
+    '/web_dev/Projects/WildeklawerTydkaarte/CSS/login.css',
+    '/web_dev/Projects/WildeklawerTydkaarte/CSS/bootstrap.min.css',
+    '/web_dev/Projects/WildeklawerTydkaarte/site_Privacy.html',
+    '/web_dev/Projects/WildeklawerTydkaarte/site_Terms.html',
+    '/web_dev/Projects/WildeklawerTydkaarte/js/bootstrap.bundle.min.js',
+    '/web_dev/Projects/WildeklawerTydkaarte/Img/icon-192x192.png'
+];
 
 self.addEventListener('install', (event) => {
+    // Load Offline Error Page for uncashed php pages
     event.waitUntil((async () => {
         const cache = await caches.open(CACHE_NAME);
-        // Setting {cache: 'reload'} in the new request will ensure that the response
-        // isn't fulfilled from the HTTP cache; i.e., it will be from the network.
         await cache.add(new Request(OFFLINE_URL, { cache: 'reload' }));
+    })());
+
+    // Load Offline cash for PWA
+    event.waitUntil((async () => {
+        const cache = await caches.open(CACHE_NAME);
+        await cache.addAll(filesToCache);
     })());
 });
 
@@ -35,44 +39,39 @@ self.addEventListener('activate', (event) => {
             await self.registration.navigationPreload.enable();
         }
     })());
-
     // Tell the active service worker to take control of the page immediately.
     self.clients.claim();
 });
 
 self.addEventListener('fetch', (event) => {
     // We only want to call event.respondWith() if this is a navigation request
-    // for an HTML page.
     if (event.request.mode === 'navigate') {
         event.respondWith((async () => {
             try {
-                // First, try to use the navigation preload response if it's supported.
+                // First, try to use the network preload response if it's supported.
                 const preloadResponse = await event.preloadResponse;
                 if (preloadResponse) {
                     return preloadResponse;
                 }
-
                 const networkResponse = await fetch(event.request);
                 return networkResponse;
             } catch (error) {
-                // catch is only triggered if an exception is thrown, which is likely
-                // due to a network error.
-                // If fetch() returns a valid HTTP response with a response code in
                 // the 4xx or 5xx range, the catch() will NOT be called.
-                console.log('Fetch failed; returning offline page instead.', error);
-
+                // load cache
                 const cache = await caches.open(CACHE_NAME);
-
-                // Fix This line for each url
-                const cachedResponse = await cache.match(OFFLINE_URL);
+                // load standard reponse for anything not in cache
+                const offlineResponse = await cache.match(OFFLINE_URL);
+                // load if url is cached
+                const eventRequest = await cache.match(event.request.url);
+                // check to see if we send offline or cache
+                if (eventRequest) {
+                    cachedResponse = eventRequest;
+                } else {
+                    cachedResponse = offlineResponse;
+                }
+                // send response
                 return cachedResponse;
             }
         })());
     }
-
-    // If our if() condition is false, then this fetch handler won't intercept the
-    // request. If there are any other fetch handlers registered, they will get a
-    // chance to call event.respondWith(). If no fetch handlers call
-    // event.respondWith(), the request will be handled by the browser as if there
-    // were no service worker involvement.
 });
