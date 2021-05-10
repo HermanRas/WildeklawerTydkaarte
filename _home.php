4adfc553a27750838b3aba23f34d47e4ec439cea
herman.ras.it@gmail.com <herman.ras.it@gmail.com>
2021-05-10 15:06:17 +0200

    Fixed Delete record Bug

--- _home.php
+++ _home.php
@@ -1,5 +1,5 @@
 <?php
-$msg = '';
+
 if (isset($_GET['delete'])){
     
     $tid = $_GET['delete'];
@@ -10,15 +10,37 @@ if (isset($_GET['delete'])){
     $result = sqlQuery($sql, $sqlargs);
 
     //if delete
-    $msg = '<div class="alert alert-success alert-dismissible fade show" role="alert">
+    echo   '<!-- The Page Title -->
+            <title>Wildeklawer Tydkaarte</title>
+
+            <!-- Add Manifest -->
+            <link rel="manifest" href="./manifest.json">
+            <!-- End Manifest -->
+
+            <!-- Chrome/android APP settings -->
+            <meta name="theme-color" content="#00AB93">
+            <link rel="icon" href="Img/icon.jpeg" sizes="192x192">
+            <!-- end of Chrome/Android App Settings  -->
+
+            <!-- Bootstrap // you can use hosted CDN here-->
+            <link rel="stylesheet" href="CSS/bootstrap.min.css">
+            <link rel="stylesheet" href="CSS/app.css">
+            <!-- end of bootstrap -->
+            <div class="container">
+            <div class="alert alert-success alert-dismissible fade show" role="alert">
                 <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
                 Taak Verwyder !
-            </div>';
+            </div>
+
+            <br>
+             <a href="home.html" class="btn btn-primary">Tuis</a>
+             </div>';
+            die;
+
     }
 ?>
 <h1 class="bg-info"><img style="height:1.5em;" src="Img/tuis.png" class="rounded m-1 p-1" alt="Tuis">Tuis</h1>
 <div class="container">
-    <?php echo $msg; ?>
     <a class="toggle-vis btn btn-info btn-sm my-2" data-column="0">Datum</a>
     <a class="toggle-vis btn btn-info btn-sm my-2" data-column="1">Plaas</a>
     <a class="toggle-vis btn btn-info btn-sm my-2" data-column="2">Gewas</a>
@@ -49,11 +71,11 @@ if (isset($_GET['delete'])){
                         LIMIT 0, 20;";
                 require_once 'config/db_query.php';
                 $sqlargs = array();
-                $result = sqlQuery($sql, $sqlargs);
+                $result = sqlQueryEmulate($sql, $sqlargs);
                 foreach ($result[0] as $row) {
                 ?>
                 <td>
-                    <a href="home.html?delete=<?php echo $row['id'] ?>" style="color:red;"><i
+                    <a href="_home.php?delete=<?php echo $row['id'] ?>" style="color:red;"><i
                             class="fa fa-trash-alt"></i></a>
                     <span class="small">
                         <?php echo $row['logDate'].' '.$row['logTime'];?>
