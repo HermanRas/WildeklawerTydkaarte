02cb1fceed8b5a92569375312cd90b49a330c388
herman.ras.it@gmail.com <herman.ras.it@gmail.com>
2021-05-10 11:23:27 +0200

    remove debug

--- App/backgroundSync.js
+++ App/backgroundSync.js
@@ -5,13 +5,11 @@ function doDBSync() {
         let baseURL = 'https://phq-7hxllh2.petragroup.local/web_dev/Projects/WildeklawerTydkaarte';
         let apiKey = 'MucJIL1vkG6YJibwB7HINgvnT89gpK';
 
-
         // if clockingsUP local DB is created
         if (localStorage.getItem('worklogUP')) {
             // Load Offline clockingsUP
             let records = JSON.parse(localStorage.getItem('clockingsUP'));
             if (records.length !== 0) {
-                //console.log(records[0]);
                 postDBUpdate(baseURL + '/API/clockUp.php' + '?KEY=' + apiKey, 'clockingsUP')
             }
         }
@@ -21,10 +19,10 @@ function doDBSync() {
             // Load Offline worklogUP
             let records = JSON.parse(localStorage.getItem('worklogUP'));
             if (records.length !== 0) {
-                console.log(records[0]);
                 postDBUpdate(baseURL + '/API/workUp.php' + '?KEY=' + apiKey, 'worklogUP')
             }
         }
     }
 }
+
 doDBSync();
