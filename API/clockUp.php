58684c3eae843e25f1593e980ac6876046ede6be
herman.ras.it@gmail.com <herman.ras.it@gmail.com>
2021-07-08 16:53:09 +0200

    Added response header before processing

--- API/clockUp.php
+++ API/clockUp.php
@@ -5,37 +5,56 @@ if (isset($_GET['KEY'])){
     if ($_GET['KEY'] == 'MucJIL1vkG6YJibwB7HINgvnT89gpK'){
     // var_dump($_POST);
 
-    for ($i=0; $i < count($_POST['cn']); $i++) {
-        $uid = $_POST["user_id"][$i];
-        $Plaas = $_POST["farm_id"][$i];
-        $CN = $_POST["cn"][$i];
-        $Spry = $_POST["spry_id"][$i];
-        $clockType = $_POST["clockType"][$i];
-        $Gewas = $_POST["produce_id"][$i];
-        $task = $_POST["task_id"][$i];
-        $date = $_POST["logDate"][$i];
-        $time = $_POST["logTime"][$i];
+        // Set header for free run, send response then process
+        ignore_user_abort(true);//not required
+        set_time_limit(60);
+        // Buffer all upcoming output...
+        ob_start();
+        // Send your response.
+        $info = array('success'=>'DataSaved');
+        echo   json_encode($info);
+        // Get the size of the output.
+        $size = ob_get_length();
+        // Disable compression (in case content length is compressed).
+        header("Content-Encoding: none");
+        // Set the content length of the response.
+        header("Content-Length: {$size}");
+        // Close the connection.
+        header("Connection: close");
+        // Flush all output.
+        ob_end_flush();
+        ob_flush();
+        flush();
 
-        // Lookup user_id
-        $sql = "select id,naam,van from `workers` where CN = '$CN' limit 1;";
+        // After response processing 
+        for ($i=0; $i < count($_POST['cn']); $i++) {
+            $uid = $_POST["user_id"][$i];
+            $Plaas = $_POST["farm_id"][$i];
+            $CN = $_POST["cn"][$i];
+            $Spry = $_POST["spry_id"][$i];
+            $clockType = $_POST["clockType"][$i];
+            $Gewas = $_POST["produce_id"][$i];
+            $task = $_POST["task_id"][$i];
+            $date = $_POST["logDate"][$i];
+            $time = $_POST["logTime"][$i];
 
-        require_once 'config/db_query.php';
-        $sqlargs = array();
-        $worker_id = sqlQuery($sql, $sqlargs);
-        $worker_id = $worker_id[0][0]['id'];
+            // Lookup user_id
+            $sql = "select id,naam,van from `workers` where CN = '$CN' limit 1;";
+
+            require_once 'config/db_query.php';
+            $sqlargs = array();
+            $worker_id = sqlQuery($sql, $sqlargs);
+            $worker_id = $worker_id[0][0]['id'];
 
 
-        // add worker to clock log
-        $sql = "insert into clocklog (user_id,worker_id,farm_id,spry_id,produce_id,task_id,clockType,logDate,logTime) 
-                values('$uid','$worker_id',     '$Plaas',    '$Spry','$Gewas',   '$task', $clockType,'$date', '$time');";
-        // echo $sql;
-        require_once 'config/db_query.php';
-        $sqlargs = array();
-        $res = sqlQuery($sql, $sqlargs);
-    }
-
-    $info = array('success'=>'DataSaved');
-    echo   json_encode($info);
+            // add worker to clock log
+            $sql = "insert into clocklog (user_id,worker_id,farm_id,spry_id,produce_id,task_id,clockType,logDate,logTime) 
+                    values('$uid','$worker_id',     '$Plaas',    '$Spry','$Gewas',   '$task', $clockType,'$date', '$time');";
+            // echo $sql;
+            require_once 'config/db_query.php';
+            $sqlargs = array();
+            $res = sqlQuery($sql, $sqlargs);
+        }
     }else{
         http_response_code(403);
         $err = array('error'=>'API AUTH KEY FAILED');
--- API/workUp.php
+++ API/workUp.php
@@ -3,37 +3,57 @@
 if (isset($_GET['KEY'])){
     // TEST to see if this is our key
     if ($_GET['KEY'] == 'MucJIL1vkG6YJibwB7HINgvnT89gpK'){
-    
-    for ($i=0; $i < count($_POST['worker_id']); $i++) {
-        $uid = $_POST["user_id"][$i];
-        $CN = $_POST["worker_id"][$i];
-        $Plaas = $_POST["farm_id"][$i];
-        $Gewas = $_POST["produce_id"][$i];
-        $Spry = $_POST["spry_id"][$i];
-        $kratte = $_POST["crates"][$i];
-        $task = $_POST["task_id"][$i];
-        $date = $_POST["logDate"][$i];
-        $time = $_POST["logTime"][$i];
 
-        // Lookup user_id
-        $sql = "select id,naam,van from `workers` where CN = '$CN' limit 1;";
+        // Set header for free run, send response then process
+        ignore_user_abort(true);//not required
+        set_time_limit(60);
+        // Buffer all upcoming output...
+        ob_start();
+        // Send your response.
+        $info = array('success'=>'DataSaved');
+        echo   json_encode($info);
+        // Get the size of the output.
+        $size = ob_get_length();
+        // Disable compression (in case content length is compressed).
+        header("Content-Encoding: none");
+        // Set the content length of the response.
+        header("Content-Length: {$size}");
+        // Close the connection.
+        header("Connection: close");
+        // Flush all output.
+        ob_end_flush();
+        ob_flush();
+        flush();
 
-        require_once 'config/db_query.php';
-        $sqlargs = array();
-        $worker_id = sqlQuery($sql, $sqlargs);
-        $worker_id = $worker_id[0][0]['id'];
-        
-        $sql = "insert into worklog (user_id,worker_id,farm_id,produce_id,spry_id,task_id,crates,logDate,logTime)
-        values('$uid','$worker_id',     '$Plaas', '$Gewas',    '$Spry',   '$task', '$kratte','$date', '$time');";
-        
-        require_once 'config/db_query.php';
-        $sqlargs = array();
-        $res = sqlQuery($sql, $sqlargs);
-        
-    }
-    
-    $info = array('success'=>'DataSaved');
-    echo   json_encode($info);
+        // After response processing 
+        for ($i=0; $i < count($_POST['worker_id']); $i++) {
+            $uid = $_POST["user_id"][$i];
+            $CN = $_POST["worker_id"][$i];
+            $Plaas = $_POST["farm_id"][$i];
+            $Gewas = $_POST["produce_id"][$i];
+            $Spry = $_POST["spry_id"][$i];
+            $kratte = $_POST["crates"][$i];
+            $task = $_POST["task_id"][$i];
+            $date = $_POST["logDate"][$i];
+            $time = $_POST["logTime"][$i];
+
+            // Lookup user_id
+            $sql = "select id,naam,van from `workers` where CN = '$CN' limit 1;";
+
+            require_once 'config/db_query.php';
+            $sqlargs = array();
+            $worker_id = sqlQuery($sql, $sqlargs);
+            $worker_id = $worker_id[0][0]['id'];
+            
+            $sql = "insert into worklog (user_id,worker_id,farm_id,produce_id,spry_id,task_id,crates,logDate,logTime)
+            values('$uid','$worker_id',     '$Plaas', '$Gewas',    '$Spry',   '$task', '$kratte','$date', '$time');";
+            
+            require_once 'config/db_query.php';
+            $sqlargs = array();
+            $res = sqlQuery($sql, $sqlargs);
+            
+        }
+
     }else{
         http_response_code(403);
         $err = array('error'=>'API AUTH KEY FAILED');
