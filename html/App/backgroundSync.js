9596088b3fb11c1669769c858e61cc26b18b4495
Jan Hendrik Rust <janr@resettech.co.za>
2023-03-13 17:35:49 +0200

    Background Sync calls Service Worker to do actual Sync

--- html/App/backgroundSync.js
+++ html/App/backgroundSync.js
@@ -1,63 +1,18 @@
 function doDBSync() {
-    var date = new Date();
-    date.setHours(date.getHours() + 2);
-    date = date.toISOString().substring(14, 16);
-    if ((date > 00 && date < 05) || (date > 30 && date < 35)) {
-        console.log(date)
-        // if we online
-        if (window.navigator.onLine) {
-
-            // set api stuff
-            let baseURL = 'https://laptop.dev:8443';
-            let apiKey = 'MucJIL1vkG6YJibwB7HINgvnT89gpK';
-
-            // if clockingsUP local DB is created
-            if (localStorage.getItem('worklogUP')) {
-                // Load Offline clockingsUP
-                let records = JSON.parse(localStorage.getItem('clockingsUP'));
-                if (records.length !== 0) {
-                    postDBUpdate(baseURL + '/API/clockUp.php' + '?KEY=' + apiKey, 'clockingsUP')
-                }
-            }
-
-            // if worklogUP local DB is created
-            if (localStorage.getItem('worklogUP')) {
-                // Load Offline worklogUP
-                let records = JSON.parse(localStorage.getItem('worklogUP'));
-                if (records.length !== 0) {
-                    postDBUpdate(baseURL + '/API/workUp.php' + '?KEY=' + apiKey, 'worklogUP')
-                }
-            }
-        }
+    console.log("DB Sync Called");
+    // if we're online
+    if (checkConnectionQuality() == true) {
+        const broadcast = new BroadcastChannel('wildeklawer');
+        broadcast.postMessage({ type: 'do-background-sync', });
     }
 }
 doDBSync();
 
 function fSync() {
+    console.log("Force Sync Called");
     // if we online
-    if (window.navigator.onLine) {
-
-        alert('Online - Sync Started');
-        // set api stuff
-        let baseURL = 'https://laptop.dev:8443';
-        let apiKey = 'MucJIL1vkG6YJibwB7HINgvnT89gpK';
-
-        // if clockingsUP local DB is created
-        if (localStorage.getItem('worklogUP')) {
-            // Load Offline clockingsUP
-            let records = JSON.parse(localStorage.getItem('clockingsUP'));
-            if (records.length !== 0) {
-                postDBUpdate(baseURL + '/API/clockUp.php' + '?KEY=' + apiKey, 'clockingsUP')
-            }
-        }
-
-        // if worklogUP local DB is created
-        if (localStorage.getItem('worklogUP')) {
-            // Load Offline worklogUP
-            let records = JSON.parse(localStorage.getItem('worklogUP'));
-            if (records.length !== 0) {
-                postDBUpdate(baseURL + '/API/workUp.php' + '?KEY=' + apiKey, 'worklogUP')
-            }
-        }
+    if (checkConnectionQuality() == true) {
+        const broadcast = new BroadcastChannel('wildeklawer');
+        broadcast.postMessage({ type: 'do-background-sync', });
     }
 }
