7e6e8688e660c7ec478d47d9dc16051007bf0423
Jan Hendrik Rust <janr@resettech.co.za>
2023-03-13 17:36:45 +0200

    Migrated WorkLogs and ClockLogs to Service Worker. Service Worker does synchronization to backend API

--- html/service-worker.js
+++ html/service-worker.js
@@ -1,14 +1,164 @@
+function swCheckConnectionQuality(event_details){
+
+    if (!navigator.onLine){
+        return false;
+    } else {
+
+        const goodConnection    = ['3g', '4g', '5g', '6g'];
+        const connectionType    = navigator.connection.effectiveType;
+        const connectionLatency = navigator.connection.rtt;
+        const bandwidth = navigator.connection.downlink;
+            
+        if( (connectionLatency < 500) 
+        &&  (goodConnection.includes(connectionType))
+        &&  (bandwidth > 0.5)
+        ){
+            return true;
+
+        } else {
+            return false;
+        }
+    }
+}
+
+
+async function swPostDBUpdate(URL, dataset){
+    let postData = ''
+    let i = 0;
+    dataset.forEach(row => {
+        for (const [key, value] of Object.entries(row)) {
+            ////console.log(key + "[" + i + "]", value);
+            postData += ('&' + key + '[]=' + value);
+        }
+        i++;
+    });
+
+    const requestOptions = {
+        method: 'POST',
+        headers: { 'Content-type': 'application/x-www-form-urlencoded' },
+        body: postData
+    };
+    
+    //console.dir({'SW_postDBUpdate': {'requestOptions': requestOptions}});
+
+    try {
+        const response = await fetch(URL, requestOptions);
+        const data = await response.json();
+        console.dir({'SW_postDBUpdate': {'json_response': data, 'response': response}});
+        return true;
+    } catch (error) {
+        console.error('SW_postDBUpdate Error: ' + error);
+        //console.dir({'SW_postDBUpdate': {"error": error}});
+        return false;
+    }
+
+}
+
+
 ///////////////////////////////////////////////////////////////////////////
-// BACKGROUND SYNC CODE
+// BACKGROUND SYNC CODE         https://github.com/WICG/background-sync/blob/main/explainers/sync-explainer.md
 ///////////////////////////////////////////////////////////////////////////
 self.addEventListener('sync', function (event) {
-    if (event.tag == 'DBSync') {
-        event.waitUntil(doDBSync());
+    if (event.tag == 'OneOffSync') {
+        //console.dir({"SW_sync": "OneOffSync received"});
+        event.waitUntil(swDoDBSync());
     }
 });
 
-async function doDBSync() {
-    // 
+self.addEventListener('periodicsync', function(event) {
+    if (event.registration.tag == 'PeriodicSync') {
+        //console.dir({"SW_periodicsync": "PeriodicSync received"});
+        event.waitUntil(swDoDBSync());
+    } else {
+      // unknown sync, may be old, best to unregister
+      event.registration.unregister();
+    }
+});
+
+
+async function swDoDBSync() {
+    //console.dir({"SW_doDBSync": "started"});
+    const syncBroadcast = new BroadcastChannel('wildeklawer');
+
+    if (swCheckConnectionQuality() == true) {
+
+        const DB_NAME       = 'wildeklawer';
+        const DB_STORE_NAME = 'dataset';
+        const DB_VERSION    = 1; 
+        const BASE_URL      = 'https://laptop.dev:8443';
+        const API_KEY       = 'MucJIL1vkG6YJibwB7HINgvnT89gpK';
+
+        let open_req = indexedDB.open(DB_NAME, DB_VERSION);
+        let db;
+
+        open_req.onsuccess = function (open_event) {
+            
+            db = open_event.target.result;
+
+            let transaction  = db.transaction(DB_STORE_NAME, 'readwrite');
+            let objectStore  = transaction.objectStore(DB_STORE_NAME);
+            let worklog_req  = objectStore.get('worklogUP');
+            let clocklog_req = objectStore.get('clockingsUP');
+            
+
+            worklog_req.onsuccess = function (evt) {
+                let worklogs = evt.target.result;
+                
+                if (worklogs && worklogs.length !== 0) {
+                    if(swPostDBUpdate(BASE_URL + '/API/workUp.php' + '?KEY=' + API_KEY, worklogs)){
+                        objectStore.put([],'worklogUP');
+                        syncBroadcast.postMessage({ type: 'background-sync-done', });
+                    } else {
+                        syncBroadcast.postMessage({ type: 'background-sync-not-done', });
+                    }
+                }
+
+            }
+
+            worklog_req.onerror = function (evt) {
+                console.error("get worklogUP:", evt.target.errorCode);
+            }
+
+            clocklog_req.onsuccess = function (evt) {
+                let clocklogs = evt.target.result;
+                
+                if (clocklogs && clocklogs.length !== 0) {
+                    if(swPostDBUpdate(BASE_URL + '/API/clockUp.php' + '?KEY=' + API_KEY, clocklogs)){
+                        objectStore.put([],'clockingsUP');
+                        syncBroadcast.postMessage({ type: 'background-sync-done', });
+                    } else {
+                        syncBroadcast.postMessage({ type: 'background-sync-not-done', });
+                    }
+                }
+
+            }
+
+            clocklog_req.onerror = function (evt) {
+                console.error("get clockingsUP:", evt.target.errorCode);
+            }
+
+            try {
+                db.close();
+            } catch(error) {
+                console.error(error);
+            }
+
+        };
+
+        open_req.onerror = function (event) {
+            console.error("openDb:", event.target.errorCode);
+        };
+
+        open_req.onupgradeneeded = function (event) {
+            //console.log("openDb.onupgradeneeded");
+            var store = event.currentTarget.result.createObjectStore(DB_STORE_NAME);
+        };
+    
+    } else {
+        syncBroadcast.postMessage({ type: 'background-sync-not-done', });
+    }
+
+    //console.dir({"SW_doDBSync": "completed"});
 }
 
 
@@ -155,3 +305,11 @@ function clearCash(CACHE_NAME) {
     caches.delete(CACHE_NAME);
     alert('Offline Data Cleared');
 }
+
+const swBroadcast = new BroadcastChannel('wildeklawer');
+
+swBroadcast.onmessage = (event) => {
+    if (event.data && event.data.type === 'do-background-sync') {
+      swDoDBSync();
+    }
+};
