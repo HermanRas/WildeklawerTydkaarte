25d5e213607ce8277b73a1778f1bda1b50dbbdcb
Quentin Slabbert <quentins@resettech.co.za>
2023-03-14 14:30:44 +0200

    Corrected insert statment and date reference Addded Try/Catch + error logs

--- html/API/client.php
+++ html/API/client.php
@@ -1,6 +1,8 @@
 <?php
 // TEST to see if API is present
 if (isset($_GET['KEY'])){
+    var_dump($_POST);
+    error_log(print_r($_POST,true),3,"/tmp/php.log");
     // TEST to see if this is our key
     if ($_GET['KEY'] == 'MucJIL1vkG6YJibwB7HINgvnT89gpK'){
 
@@ -9,19 +11,24 @@ if (isset($_GET['KEY'])){
         set_time_limit(60);
         // Buffer all upcoming output...
         ob_start();
-        // After response processing 
-        $uid        = $_POST["uid"];
-        $naam       = $_POST["naam"];
-        $kdatum     = $_POST["kdatum"];
-        
-        $sql = "insert into clients (uid, naam, kdatum)
-        '$uid','$naam', '$kdatums' from worklog;";
-        
-        require_once 'config/db_query.php';
-        $sqlargs = array();
-        $res = sqlQuery($sql, $sqlargs);
-        echo json_encode($res[0]);
-        // Get the size of the output.
+        try {
+            // After response processing 
+            $uid        = $_POST["uid"];
+            $naam       = $_POST["naam"];
+            $kdatum     = $_POST["kdatum"];
+            
+            $sql = "insert into clients (uid, naam, kdatum) values ('$uid','$naam', '$kdatum');";
+            
+            require_once 'config/db_query.php';
+            $sqlargs = array();
+            $res = sqlQuery($sql, $sqlargs);
+            error_log(print_r($res,true),3,"/tmp/php.log");
+            // Get the size of the output.
+        } catch (Exception $ex){
+            http_response_code(500);
+            error_log(print_r($res,true),3,"/tmp/php.log");
+        }
+    
         $size = ob_get_length();
         // Disable compression (in case content length is compressed).
         header("Content-Encoding: none");
@@ -33,7 +40,6 @@ if (isset($_GET['KEY'])){
         ob_end_flush();
         ob_flush();
         flush();
-
     }else{
         http_response_code(403);
         $err = array('error'=>'API AUTH KEY FAILED');
